9461482c47372921ce0b1584ff9b124f4907767a
Tushar Sahani <tusharsahani000@gmail.com>
2022-10-01 23:13:40 +0530

    Palindrome

---
+++ java-sol/palindrome.java
@@ -0,0 +1,24 @@
+// Palindrom-Number
+
+// https://leetcode.com/problems/palindrome-number/
+
+package Java;
+
+public class palindrome {
+
+    public static void main(String[] args) {
+        int x = -121;
+        boolean result = palindrome(x);
+        System.out.println(result);
+    }
+    static boolean palindrome(int x){
+        int n = 0;
+        int m = Math.abs(x);
+        while(x!=0){
+            int i = x%10;
+            n = n*10+i;
+            x=x/10;
+        }
+        return n==m?true:false;
+    }
+}
