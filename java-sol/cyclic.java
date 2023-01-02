0f3aafb8354474dc010868f697a08e85dd846875
avinashtiwari <tusharsahani000@gmail.com>
2023-01-03 00:34:16 +0530

    missingnumber

--- java-sol/cyclic.java
+++ java-sol/cyclic.java
@@ -1,6 +1,3 @@
-package Java.sorting;
-
-import java.util.Arrays;
 
 public class cyclic {
     public static void main(String[] args) {
---
+++ java-sol/missingnumber.java
@@ -0,0 +1,26 @@
+// link - https://leetcode.com/problems/missing-number/
+
+class solution {
+    public int missingNumber(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] < nums.length && i != nums[i]) {
+                swap(nums, i, nums[i]);
+                continue;
+            } else {
+                i++;
+            }
+        }
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j) {
+                return j;
+            }
+        }
+        return nums.length;
+    }
+    static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
