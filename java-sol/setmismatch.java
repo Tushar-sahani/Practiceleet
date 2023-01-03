a3960282fac3cf7e904c60752f94b33c733c37c6
avinashtiwari <tusharsahani000@gmail.com>
2023-01-03 22:53:02 +0530

    Create setmismatch.java

---
+++ java-sol/setmismatch.java
@@ -0,0 +1,27 @@
+// LINK - https://leetcode.com/problems/set-mismatch/
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int[] arr = new int[2];
+        int i=0;
+        while(i<nums.length){
+            if(nums[nums[i]-1] != nums[i]){
+                swap(nums,i,nums[i]-1);
+            }
+            else{
+                i++;
+            }
+        }
+        for(int j=0;j<nums.length;j++){
+            if(j != nums[j]-1){
+                arr[0] = nums[j];
+                arr[1] = j+1;
+            }
+        }
+        return arr;
+    }
+    static void swap(int[] nums,int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
