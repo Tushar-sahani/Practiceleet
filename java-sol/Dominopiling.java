f6882cc24d26abc63f46d4d87dcec12cac1d8717
avinashtiwari <tusharsahani000@gmail.com>
2023-01-05 22:50:54 +0530

    Create Dominopiling.java

---
+++ java-sol/Dominopiling.java
@@ -0,0 +1,11 @@
+// link - https://codeforces.com/problemset/problem/50/A
+
+import java.util.*;
+public class Dominopiling {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        System.out.println((int)Math.ceil((m*n)/2));
+    }
+}
