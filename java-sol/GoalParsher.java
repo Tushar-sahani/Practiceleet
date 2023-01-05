3bd5a1b29b8e556694e9753d68d79c747396a25d
avinashtiwari <tusharsahani000@gmail.com>
2023-01-05 15:22:00 +0530

    Create GoalParsher.java

---
+++ java-sol/GoalParsher.java
@@ -0,0 +1,24 @@
+// link - https://leetcode.com/problems/goal-parser-interpretation/
+public class GoalParsher {
+    public static void main(String[] args) {
+        String command = "()(al)G()()(al)";
+        char[] str = command.toCharArray();
+        int i=0;
+        String str2 = "";
+        while(i<str.length){
+            if(str[i]=='('&& str[i+1]==')'){
+                str2 += "o";
+                i = i + 2;
+            }
+            else if(str[i]=='(' && str[i+1]=='a'){
+                str2 +="al";
+                i = i + 4;
+            }
+            else{
+                str2 += str[i]; 
+                i++;
+            }
+        }
+        System.out.println(str2);
+    }
+}
