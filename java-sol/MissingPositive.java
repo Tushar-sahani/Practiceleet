8cdd94aee0d8e4f263c02cf5d0ea04c7c522e87c
avinashtiwari <tusharsahani000@gmail.com>
2023-01-05 15:28:29 +0530

    Create MissingPositive.java

---
+++ java-sol/MissingPositive.java
@@ -0,0 +1,26 @@
+// link - https://leetcode.com/problems/first-missing-positive/
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while(i<nums.length){
+            if(nums[i]>0 &&nums[i]<=nums.length && nums[nums[i]-1] != nums[i] ){
+                swap(nums,i,nums[i]-1);
+            }
+            else{
+                i++;
+            }
+        }
+        for(int j=0;j<nums.length;j++){
+            if( j != nums[j]-1){
+                return j+1;
+            }
+        }
+    return nums.length+1;
+    }
+    static void swap(int[] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
