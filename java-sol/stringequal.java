5d4e025b1bdd2c89236b88870cbcc5a2c80f221a
avinashtiwari <tusharsahani000@gmail.com>
2023-01-06 23:57:23 +0530

    Create stringequal.java

---
+++ java-sol/stringequal.java
@@ -0,0 +1,17 @@
+// link - https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent
+
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String str1 = "";
+        String str2 = "";
+        for(int i=0;i<word1.length;i++){
+            str1 += word1[i];
+        }
+        for(int i=0;i<word2.length;i++){
+            str2 += word2[i];
+        }
+        
+        return str1.equals(str2);
+        
+    }
+}
