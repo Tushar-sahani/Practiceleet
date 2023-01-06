571cfd219436ac2537d196b106a66a77cae52db0
avinashtiwari <tusharsahani000@gmail.com>
2023-01-06 23:43:00 +0530

    Create kthlargest.java

---
+++ java-sol/kthlargest.java
@@ -0,0 +1,28 @@
+
+public class kthlargest {
+    public static void main(String[] args) {
+        int[] arr = {3,2,3,1,2,4,5,5,6};
+        int a = findKthLargest(arr,4);
+        System.out.println(a);
+    }
+    static int findKthLargest(int[] nums, int k) 
+    {
+        int max = nums[0];
+        for (int i=0;i<nums.length;i++) {
+            if(nums[i]>max){
+                max = nums[i];
+            }
+        }
+        int count = 0;
+        int i=0;
+        int b = max;
+        while(max>i){
+            if(b>=k)
+            {
+                count++;
+            }
+            i++;
+        }
+        return count;
+    }
+}
