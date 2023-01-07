7a2c037e24defa05ba2d779014a7798134210c39
avinashtiwari <tusharsahani000@gmail.com>
2023-01-08 00:50:26 +0530

    Create integerMapping.java

---
+++ integerMapping.java
@@ -0,0 +1,20 @@
+public class integerMapping {
+    public static void main(String[] args) {
+        String s = "1326#";
+        StringBuilder sb = new StringBuilder();
+        int i = s.length()-1;
+        while(i>=0){
+            if(s.charAt(i) != '#'){
+                int n = s.charAt(i) - '0';
+                sb.append((char)(96+n));
+                i--;
+            }else{
+                String str = s.substring(i-2, i);
+                int m = (str.charAt(0)-'0')*10 + (str.charAt(1) - '0');
+                sb.append((char)(96+m));
+                i=i-3;
+            }
+        }
+        System.out.println(sb.reverse());
+    }
+}
