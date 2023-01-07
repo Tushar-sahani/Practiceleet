4a624cd4169ea569cc294c5f98d3c178b75a0143
avinashtiwari <tusharsahani000@gmail.com>
2023-01-07 18:03:47 +0530

    Create AlikeString.java

---
+++ java-sol/AlikeString.java
@@ -0,0 +1,21 @@
+// link - https://leetcode.com/problems/determine-if-string-halves-are-alike/
+
+class Solution {
+    public boolean halvesAreAlike(String s) {
+       char[] str = {'a','e','i','o','u','A','E','I','O','U'};
+       String sc = String.valueOf(str);
+       String a = s.substring(0, (s.length())/2);
+       String b = s.substring((s.length())/2, s.length());
+       int count1 =0,count2 =0;
+       for (int i = 0; i < (s.length())/2; i++) {
+            if(sc.contains(""+a.charAt(i))==true){
+                count1++;
+            }
+            if(sc.contains(""+b.charAt(i))==true){
+                count2++;
+            }
+       }
+       
+       return (count1==count2)?true:false; 
+    }
+}
