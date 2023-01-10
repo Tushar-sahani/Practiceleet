c7d9cbed0b9712eef12a2300e2b44aa3930f5b0f
avinashtiwari <tusharsahani000@gmail.com>
2023-01-10 22:48:54 +0530

    Create sum1.java

---
+++ sum1.java
@@ -0,0 +1,16 @@
+package Java.Practiceleet;
+public class sum1 {
+    public int[] plusOne(int[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] < 9) {
+                arr[i]++;
+                return arr;
+            }
+            arr[i] = 0;
+        }
+        
+        arr = new int[arr.length + 1];
+        arr[0] = 1;
+        return arr;
+    }
+}
